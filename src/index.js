6f4bb543e2fbea3de4b958728b420f5af064fc23
jkhaha <jha2@wellesley.edu>
2019-02-25 17:45:40 -0500

    removes 4 cards without adding

--- src/index.js
+++ src/index.js
@@ -22,20 +22,13 @@ let numbers = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'K
   function dealCards(){
     for (let i=0; i < 4; i++){
       var cards = deck[Math.floor(Math.random()*deck.length)];
+
       fourCards.push(cards)
     }
-    for (let j=0; j< fourCards.length; j++){
-      let oneCard = document.createElement('div');
-      let number = document.createElement('div');
-      let suit = document.createElement('div');
-      number.innerHTML = fourCards[j].number
 
-      oneCard.append(number);
-      oneCard.append(suit);
-      cardDiv.append(oneCard)
-    }
     deck = deck.filter(card => !fourCards.includes(card))
     console.log(fourCards, deck);
+    fourCards = []
   }
 
 
